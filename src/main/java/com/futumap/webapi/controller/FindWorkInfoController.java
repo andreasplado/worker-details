ee5a0da5f9a4567da66383d6df2853320dccca0e
Andreas Plado <andreasplado@gmail.com>
2020-10-27 00:11:43 +0200

    Refactoring

--- src/main/java/com/futumap/webapi/controller/FindWorkInfoController.java
+++ src/main/java/com/futumap/webapi/controller/FindWorkInfoController.java
@@ -4,17 +4,18 @@ import com.futumap.webapi.dao.entity.UserEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class FindWorkInfoController {
-    @GetMapping
+    @RequestMapping("/")
     public String mainEntry() {
         return "index";
     }
-    @GetMapping("/my_profile")
+    @RequestMapping("/my_profile")
     public String myProfile() {
         return "my_profile";
     }
