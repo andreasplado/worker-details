28a8d25af2bca740185dff6126d0590a3eaef4df
Andreas Plado <andreasplado@gmail.com>
2020-10-30 01:40:56 +0200

    Refactoring

--- src/main/java/com/futumap/webapi/service/ExcelService.java
+++ src/main/java/com/futumap/webapi/service/ExcelService.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ExcelService implements IExcelService {
 
