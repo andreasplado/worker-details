3c2f65fa884422dee429debbfa37f04cf0eace00
Andreas Plado <andreasplado@gmail.com>
2020-10-30 01:06:35 +0200

    Refactoring

--- src/main/java/com/futumap/webapi/controller/ExcelController.java
+++ src/main/java/com/futumap/webapi/controller/ExcelController.java
@@ -32,7 +32,7 @@ public class ExcelController {
                 message = "Uploaded the file successfully: " + file.getOriginalFilename();
                 return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
             } catch (Exception e) {
-                message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+                message = "Could not upload the file: " + file.getOriginalFilename() + "!" + " Error: " + e.getMessage();
                 return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
             }
         }
