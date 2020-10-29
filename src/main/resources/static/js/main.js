9ba61031fa5514217b2e55b381acb47796c131b7
Andreas Plado <andreasplado@gmail.com>
2020-10-29 06:34:42 +0200

    Refactoring

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -63,13 +63,14 @@
 
 
     function logOut(){
-          gapi.load('auth2', function() {
-            var auth2 = gapi.auth2.init({
-              client_id: '485896048610-rk8i4i4fkh9c1ss58207kl3ltoibpsaa.apps.googleusercontent.com',
-              // Scopes to request in addition to 'profile' and 'email'
-              //scope: 'additional_scope'
-            });
-              auth2.authorize(
+        gapi.load('auth2', function() {
+                auth2 = gapi.auth2.init({
+                  client_id: '485896048610-rk8i4i4fkh9c1ss58207kl3ltoibpsaa.apps.googleusercontent.com',
+                  // Scopes to request in addition to 'profile' and 'email'
+                  //scope: 'additional_scope'
+                });
+              });
+        gapi.authorize(
                                   {
                                       'client_id': '485896048610-rk8i4i4fkh9c1ss58207kl3ltoibpsaa.apps.googleusercontent.com',
                                   },
